3f94d68f0ca6a4f3ba47c9d70776c9e6f1ef8271
wutaihao <th-wu@zakj.info>
2018-01-25 16:32:25 +0800

    使用war包的形式运行项目,修改内置Tomcat为jetty

--- listedfacesys/src/main/java/com/cscs/listedfacesys/controller/HelloTestController.java
+++ listedfacesys/src/main/java/com/cscs/listedfacesys/controller/HelloTestController.java
@@ -2,7 +2,6 @@ package com.cscs.listedfacesys.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloTestController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
     public String hello(){
         return "Hello World!";
     }
