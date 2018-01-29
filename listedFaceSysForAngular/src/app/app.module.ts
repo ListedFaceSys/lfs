386e2fdd5cf71324dfe493b63af8ffc5a8ced97c
wutaihao <th-wu@zakj.info>
2018-01-29 17:44:27 +0800

    创建上市展台

--- listedFaceSysForAngular/src/app/app.module.ts
+++ listedFaceSysForAngular/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from "./app-routing.module";
-import { CompanyShowModule } from "./company-show/company-show.module";
+import { AppRoutingModule } from './app-routing.module';
+import { CompanyShowModule } from './company-show/company-show.module';
 
-import { ErrorComponent } from "./public/error/error.component";
-import { LoginComponent } from "./public/login/login.component";
-import { HeaderComponent } from "./public/header/header.component";
-import { MenuComponent } from "./public/menu/menu.component";
-import { IsLastDirective } from "./common/utill/isLast";
+import { ErrorComponent } from './public/error/error.component';
+import { LoginComponent } from './public/login/login.component';
+import { HeaderComponent } from './public/header/header.component';
+import { MenuComponent } from './public/menu/menu.component';
+import { IsLastDirective } from './common/utill/isLast';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,      //数据的双向绑定 [(ngModel)]
+    AppRoutingModule,
+    CompanyShowModule
+  ],
   declarations: [
     AppComponent,
     ErrorComponent,   //报错模块
@@ -20,12 +26,6 @@ import { IsLastDirective } from "./common/utill/isLast";
     HeaderComponent,  //头, BaseComponent部模块
     MenuComponent,    //菜单模块
     IsLastDirective   //指令
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,      //数据的双向绑定 [(ngModel)]
-    AppRoutingModule,
-    CompanyShowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- listedFaceSysForAngular/src/app/company-show/company-show.module.ts
+++ listedFaceSysForAngular/src/app/company-show/company-show.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BaseComponent } from './base/base.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    BaseComponent
+  ]
 })
 export class CompanyShowModule { }
