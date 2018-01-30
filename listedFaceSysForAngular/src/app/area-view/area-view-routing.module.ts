1cf12ea05e3c0ad2b369ea6e2a6618457f3d090b
wutaihao <th-wu@zakj.info>
2018-01-30 19:39:10 +0800

    创建地图demo

--- listedFaceSysForAngular/src/app/area-view/area-view-routing.module.ts
+++ listedFaceSysForAngular/src/app/area-view/area-view-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AreaViewComponent } from "./area-view.component";
+
+const routes: Routes = [
+  { path: 'areaView', component: AreaViewComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class AreaViewRoutingModule { }
--- listedFaceSysForAngular/src/app/company-show/company-show-routing.module.ts
+++ listedFaceSysForAngular/src/app/company-show/company-show-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import {CompanyShowComponent} from './company-show.component';
+import { CompanyShowComponent } from './company-show.component';
 
 const routes: Routes = [
   { path: 'companyShow', component: CompanyShowComponent }
