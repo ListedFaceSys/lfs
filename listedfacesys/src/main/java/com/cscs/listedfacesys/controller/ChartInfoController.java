ef14067ba3c072f45a125d0e30ae96d9b55b6ea8
wuyu616818 <wuyu616818@163.com>
2018-02-01 17:45:48 +0800

    Controller定义完成，预留一个未命名Controller

---
+++ listedfacesys/src/main/java/com/cscs/listedfacesys/controller/ChartInfoController.java
@@ -0,0 +1,16 @@
+package com.cscs.listedfacesys.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Create by wzy on 2018/2/1
+ * 图表类信息查询
+ */
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/chart")
+public class ChartInfoController {
+    
+}
---
+++ listedfacesys/src/main/java/com/cscs/listedfacesys/controller/NewsTopInfoController.java
@@ -0,0 +1,16 @@
+package com.cscs.listedfacesys.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Create by wzy on 2018/2/1
+ * TOP10类信息查询
+ */
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/newsTop")
+public class NewsTopInfoController {
+
+}
