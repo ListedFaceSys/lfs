6624b816ffab0d4373745d9d3f7511f567365985
huangjie1234 <huangjie@linkstec.com>
2018-02-01 20:58:04 +0800

    负面新闻跟踪功能 hj

--- listedfacesys/src/main/java/com/cscs/listedfacesys/controller/RegionRiskController.java
+++ listedfacesys/src/main/java/com/cscs/listedfacesys/controller/RegionRiskController.java
@@ -1,8 +1,14 @@
 package com.cscs.listedfacesys.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cscs.listedfacesys.dto.NewsTableOutData;
+import com.cscs.listedfacesys.dto.base.BaseOutData;
+import com.cscs.listedfacesys.services.NewsTableService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Create by wzy on 2018/2/1
@@ -12,5 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/regionRisk")
 public class RegionRiskController {
+    @Autowired
+    NewsTableService newsTableService;
+
+    //违约事件跟踪
+    @RequestMapping(value = "/lastingBondViolation/{page}", method = RequestMethod.GET)
+    public BaseOutData getViolation(@PathVariable int page) {
+        BaseOutData out = new BaseOutData();
+        try {
+            out =  newsTableService.getLastingBondViolationNews(page, 10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out;
+    }
 
 }
