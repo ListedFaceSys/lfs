f263cba57b6acf8518baaacd20ea9476501c2d47
wangkunmin <2239486439@qq.com>
2018-02-02 18:17:31 +0800

    区域风险模块

--- listedFaceSysForAngular/src/app/group-risk/group-risk.module.ts
+++ listedFaceSysForAngular/src/app/group-risk/group-risk.module.ts
@@ -4,12 +4,21 @@ import { CommonModule } from '@angular/common';
 import { GroupRiskComponent } from './group-risk.component';
 
 import { GroupRiskRoutingModule } from './group-risk-routing.module';
+import {GroupNewsEventComponent} from "./group-news-event/group-news-event.component";
+import {AccordionModule} from "ngx-bootstrap";
+import {NgxEchartsModule} from "ngx-echarts";
 
 @NgModule({
   imports: [
     CommonModule,
-    GroupRiskRoutingModule
+    GroupRiskRoutingModule,
+    NgxEchartsModule,
+    AccordionModule.forRoot()
   ],
-  declarations: [GroupRiskComponent]
+  declarations: [
+    GroupRiskComponent,
+    GroupNewsEventComponent,
+
+  ]
 })
 export class GroupRiskModule { }
--- listedFaceSysForAngular/src/app/region-risk/region-risk.component.ts
+++ listedFaceSysForAngular/src/app/region-risk/region-risk.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from "../common/service/login.service";
 
 @Component({
   selector: 'app-region-risk',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegionRiskComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService
+  ) { }
 
   ngOnInit() {
+    this.loginService.sendLogin(true);
   }
 
 }
