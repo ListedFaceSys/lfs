369abed85f31437accfb173b0e64f9be02ed5ece
wutaihao <th-wu@zakj.info>
2018-02-03 14:05:15 +0800

    区域风险初步完成

--- listedFaceSysForAngular/src/app/region-risk/region-risk.module.ts
+++ listedFaceSysForAngular/src/app/region-risk/region-risk.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { RegionRiskComponent } from './region-risk.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { AccordionModule } from 'ngx-bootstrap';
 
-import {GeographyComponent} from './geography/geography.component';
-import {NewsEventComponent} from './news-event/news-event.component';
-import {EarlyWarningComponent} from './early-warning/early-warning.component';
-import {NewsTrackComponent} from './news-track/news-track.component';
-import {CategoryComponent} from './category/category.component';
-import {NgxEchartsModule} from 'ngx-echarts';
-import {AccordionModule} from 'ngx-bootstrap';
-import {LoginService} from '../common/service/login.service';
-import {AreaViewApiService} from '../common/api/area-view-api.service';
+import { RegionRiskComponent } from './region-risk.component';
+import { GeographyComponent } from './geography/geography.component';
+import { TrendChartComponent } from './trend-chart/trend-chart.component';
+import { NewsEventComponent } from './news-event/news-event.component';
+import { EarlyWarningComponent } from './early-warning/early-warning.component';
+import { NewsTrackComponent } from './news-track/news-track.component';
+
+import { LoginService } from '../common/service/login.service';
+import { AreaViewApiService } from '../common/api/area-view-api.service';
 
 @NgModule({
   imports: [
@@ -21,12 +22,11 @@ import {AreaViewApiService} from '../common/api/area-view-api.service';
   ],
   declarations: [
     RegionRiskComponent,
-
-    GeographyComponent,     //上市公司地理分布一览
-    CategoryComponent,      //上市公司类别分布一览
-    EarlyWarningComponent,  //监测预警风险Top5
-    NewsEventComponent,     //热点新闻-新闻事件时间趋势图
-    NewsTrackComponent      //负面新闻跟踪
+    GeographyComponent,     // 上市公司地理分布一览
+    TrendChartComponent,    // 监测预警趋势图
+    EarlyWarningComponent,  // 监测预警新闻
+    NewsEventComponent,     // 热点新闻
+    NewsTrackComponent      // 负面新闻跟踪
   ],
   providers: [
     LoginService,
