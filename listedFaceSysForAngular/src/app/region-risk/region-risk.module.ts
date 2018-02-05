8a51b537da19374bc355c3585a7d8a98f19725e2
wutaihao <th-wu@zakj.info>
2018-02-05 15:19:07 +0800

    tooltip

--- listedFaceSysForAngular/src/app/region-risk/region-risk.module.ts
+++ listedFaceSysForAngular/src/app/region-risk/region-risk.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgxEchartsModule } from 'ngx-echarts';
-import { AccordionModule, TooltipModule, CarouselModule } from 'ngx-bootstrap';
+import { AccordionModule, CarouselModule } from 'ngx-bootstrap';
 
 import { RegionRiskComponent } from './region-risk.component';
 import { GeographyComponent } from './geography/geography.component';
@@ -19,7 +19,6 @@ import { AreaViewApiService } from '../common/api/area-view-api.service';
     CommonModule,
     NgxEchartsModule,
     AccordionModule.forRoot(),
-    TooltipModule.forRoot(),
     CarouselModule.forRoot()
   ],
   declarations: [
