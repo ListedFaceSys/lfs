4770e8c188c46ec4daf96f28d3fdf5772bbde77e
wutaihao <th-wu@zakj.info>
2018-02-07 15:00:21 +0800

    查看更多负面新闻跟踪

--- listedFaceSysForAngular/src/app/region-risk/news-track/news-track.component.ts
+++ listedFaceSysForAngular/src/app/region-risk/news-track/news-track.component.ts
@@ -1,40 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, DoCheck } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { NegativeNews } from '../../common/model/negative-news';
+import { CommonUtil } from '../../common/utill/common-util';
 
 import { BsLocaleService } from "ngx-bootstrap/datepicker";
 import { RegionRiskApiService } from '../../common/api/region-risk-api.service';
-import {CompanyNews} from "../../common/model/company-news";
+
+import { CompanyNews } from '../../common/model/company-news';
+import { NegativeNews } from '../../common/model/negative-news';
 
 @Component({
   selector: 'app-news-track',
   templateUrl: './news-track.component.html',
   styleUrls: ['../region-risk.component.css']
 })
-export class NewsTrackComponent implements OnInit {
+export class NewsTrackComponent implements OnInit, DoCheck {
   newsTrack: CompanyNews;
+  range: Date;
+  oldRange: Date;
 
   constructor(
     private router: Router,
+    private commonUtil: CommonUtil,
     private bsLocaleService: BsLocaleService,
     private regionRiskApiService: RegionRiskApiService) { }
 
   ngOnInit() {
     this.bsLocaleService.use('zh-cn');
-    this.getNewsTrack();
+    this.getNewsTrack(1, 10);
   }
 
-  getNewsTrack() {
+  ngDoCheck() {
+    if (this.range !== this.oldRange) {
+      let startDate = this.commonUtil.dateFormat(this.range[0], 'yyyy-MM-dd');
+      let endDate = this.commonUtil.dateFormat(this.range[1], 'yyyy-MM-dd');
+      console.log(startDate, endDate);
+      this.getNewsTrack(1, 10, startDate, endDate);
+      this.oldRange = this.range;
+    }
+  }
+
+  getNewsTrack(page: number, pageSize: number, startDate?: string, endDate?: string) {
     let news: NegativeNews;
     news = {
-      page: 1,
-      pageSize: 10,
+      page: page,
+      pageSize: pageSize,
+      startDate: startDate,
+      endDate: endDate
     };
     this.regionRiskApiService.getNewsTrack(news)
       .subscribe(
         data => {
-          this.newsTrack = data.data['content'];
+          if (data.code === '0') {
+            this.newsTrack = data.data['content'];
+            return;
+          }
+          this.newsTrack = null;
         },
         error => {
           console.log(error);
